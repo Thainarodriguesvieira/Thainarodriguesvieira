ad9616260a19d60d8af42e93c987eeeba0c1efd1
Thainá <thainavieira660@gmail.com>
2022-02-23 12:00:34 -0300

    Turno de estudo

---
+++ turno-estudo/turno-de-estudo/index.js
@@ -0,0 +1,28 @@
+const turno = prompt("Qual turno vocÊ estuda? \nM-Manhã \nV-Vespertino \nN-Noturno")
+
+if (turno === "M") {
+    alert("Bom dia!")
+} else if (turno === "V") {
+    alert("Boa tarde!")
+} else if (turno === "N") {
+    alert("Boa noite!")
+} else {
+    alert("Ocorreu um erro, tente novamente!")
+}
+
+// const turno = prompt("Qual turno vocÊ estuda? \nM-Manhã \nV-Vespertino \nN-Noturno")
+
+// switch (turno) {
+//     case "M":
+//         console.log("Bom dia")
+//         break
+//     case "V":
+//         console.log("Boa tarde")
+//         break
+//     case "N":
+//         console.log("Boa noite")
+//         break
+//     default:
+//         console.log("Ocorreu um erro, tente novamente")
+//         break
+// }
